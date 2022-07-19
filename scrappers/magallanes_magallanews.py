1e991988f2deaa7ce6f9dbd8f07fb769509b7f63
ManuGMO <manugm_00@hotmail.com>
2022-07-19 10:47:22 -0400

    magallanews

---
+++ scrappers/magallanes_magallanews.py
@@ -0,0 +1,69 @@
+import random
+from requests_html import HTMLSession
+import w3lib.html
+import html
+
+def format_date(date):
+        return(date.split("T")[0])
+
+session = HTMLSession()
+
+## URL que escrapear
+URL = "https://www.elmagallanews.cl/bien-comun"
+
+## Simular que estamos utilizando un navegador web
+USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+headers = {'user-agent':random.choice(USER_AGENT_LIST) }
+
+response = session.get(URL,headers=headers)
+xpath_url="//h4/a/@href"
+all_urls = response.html.xpath(xpath_url)[:4]
+n=1
+for url in all_urls:
+
+    url= "https://www.elmagallanews.cl" + url
+    print("Noticia ", n)
+    n=n+1
+    print(url)
+    response = session.get(url,headers=headers)
+
+    xpath_title='//div//h1'
+    title = response.html.xpath(xpath_title)[0].text
+    print(title)
+
+    xpath_date="//div[@class='date-created-node']//span//text()"
+    date = response.html.xpath(xpath_date)[0]
+    print(format_date(date))
+ 
+
+    xpath_text="//div[@class='content content-node']//p//text()"
+    list_p = response.html.xpath(xpath_text)
+    text=""
+    for p in list_p:
+        content = p
+        content = w3lib.html.remove_tags(content)
+        content = w3lib.html.replace_escape_chars(content)
+        content = html.unescape(content)
+        content = content.strip()
+        text=text+" "+content
+    print(text)
+    print("----------------------------------------------------------------------------------------")
